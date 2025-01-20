8b9c42a154592016a6c76f99e0cbeef5c34ab655
ramanandhaaja <thisisnandha@gmail.com>
2025-01-20 10:46:16 +0700

    update eslint

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  eslint: {
+    ignoreDuringBuilds: false,
+    dirs: ['app', 'components', 'lib', 'utils']
+  }
 };
 
 export default nextConfig;
